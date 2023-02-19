5529c50e9c6873b06e9d507ee840dfca4866ddf3
Makar <84675052+MakarUrbanov@users.noreply.github.com>
2023-02-19 21:43:15 +0400

    new file structure

---
+++ never-forget/Features/ListTab/Views/PersonsList/PersonsListView.swift
@@ -0,0 +1,20 @@
+//
+//  PersonsListView.swift
+//  never-forget
+//
+//  Created by makar on 2/19/23.
+//
+
+import SwiftUI
+
+struct PersonsListView: View {
+  var body: some View {
+    Text("Hello, World!")
+  }
+}
+
+struct PersonsListView_Previews: PreviewProvider {
+  static var previews: some View {
+    PersonsListView()
+  }
+}
